ddc09504f0c8f0ab1fc6de818943f49aa88abbc9
Alex Larson <alex.m.larson@gmail.com>
2019-03-30 09:47:57 -0700

    changed app name and css

--- Pages/Error.cshtml.cs
+++ Pages/Error.cshtml.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace my_new_app.Pages
+namespace factflux.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
