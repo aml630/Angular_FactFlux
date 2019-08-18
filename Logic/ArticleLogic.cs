96c2ded69de40d1c999be0edaae216f92d15e693
Alex Larson <alex.m.larson@gmail.com>
2019-08-18 09:25:17 -0700

    fixed bug

--- Logic/ArticleLogic.cs
+++ Logic/ArticleLogic.cs
@@ -228,7 +228,7 @@ namespace FactFluxV3.Logic
             return timeLineList;
         }
 
-        public DateTime StartOfWeek(this DateTime dt, DayOfWeek startOfWeek)
+        public DateTime StartOfWeek(DateTime dt, DayOfWeek startOfWeek)
         {
             int diff = (7 + (dt.DayOfWeek - startOfWeek)) % 7;
             return dt.AddDays(-1 * diff).Date;
